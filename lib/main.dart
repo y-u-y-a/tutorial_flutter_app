23608a1d14c9c79f1d27aec4e728cd2bf4294d9b
y-u-y-a <yuya.ymd00@gmail.com>
2020-03-28 21:44:08 +0900

    [add]body部分を追加

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,53 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text('flutterチュートリアルアプリ'),
       ),
+
+      body: Container(
+
+        child: ListView.builder(
+
+          itemCount: ChatModel.dummyData.length,
+
+          itemBuilder: (context, index) {
+
+            ChatModel _model = ChatModel.dummyData[index];
+
+            return Column(
+              children: <Widget>[
+                Divider(
+                  height: 12.0,
+                ),
+
+                ListTile(
+                  leading: CircleAvatar(
+                    radius: 24.0,
+                    backgroundImage: NetworkImage(_model.avatarUrl),
+                  ),
+
+                  title: Row(
+                    children: <Widget>[
+                      Text(_model.name),
+                      SizedBox(
+                        width: 16.0,
+                      ),
+                      Text(
+                        _model.datetime,
+                        style: TextStyle(fontSize: 12.0),
+                      ),
+                    ],
+                  ),
+
+                  subtitle: Text(_model.message),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    size: 14.0,
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
