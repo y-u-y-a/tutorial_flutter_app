1b3e67731a9c5ab7a3b554fb05810060c598fba3
y-u-y-a <yuya.ymd00@gmail.com>
2020-03-28 20:25:17 +0900

    [create/add]chat_model.dartを作成し、クラスを追加

---
+++ lib/chat_model.dart
@@ -0,0 +1,8 @@
+class ChatModel {
+  final String avatarUrl;
+  final String name;
+  final String datetime;
+  final String message;
+
+  ChatModel({this.avatarUrl, this.name, this.datetime, this.message});
+}
